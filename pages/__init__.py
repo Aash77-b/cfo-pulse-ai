072263de0c0a1e5f07f0df98123cb6149fcb25e4
Ashenafi Gebremedhin <aash7t@gmail.com>
2026-04-28 18:35:43 +0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,8 @@
+# pages/__init__.py
 
+from .dashboard import show_dashboard
+from .scan_audit import show_scan_audit
+from .fraud_reports import show_fraud_reports
+from .settings import show_settings
+from .analytics import show_analytics
+from .policies import show_policies
